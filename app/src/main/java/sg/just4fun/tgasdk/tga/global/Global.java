88f079af9085891e549de7774879a8c29699e109
jianwei <developer@just4fun.sg>
2022-02-11 10:33:07 +0800

    no message

--- app/src/main/java/sg/just4fun/tgasdk/tga/global/Global.java
+++ app/src/main/java/sg/just4fun/tgasdk/tga/global/Global.java
@@ -1,12 +1,12 @@
 package sg.just4fun.tgasdk.tga.global;
 
 public class Global {
-  public static final String YUMING="http://tgadev.just4fun.asia";//开发服
+//  public static final String YUMING="http://tgadev.just4fun.asia";//开发服
 //    public static final String YUMING="https://data.just4fun.sg";//测试服
-//  public static final String YUMING="https://tga.just4fun.sg";//正式服
+  public static final String YUMING="https://tga.just4fun.sg";//正式服
 
-    public static final String BIP_YUMING= "https://prpbipgaming.tvoip.turkcell.com.tr";//BIP测试服
-//    public static final String BIP_YUMING= "https://bipgaming.tvoip.turkcell.com.tr";//BIP正式服
+//    public static final String BIP_YUMING= "https://prpbipgaming.tvoip.turkcell.com.tr";//BIP测试服
+    public static final String BIP_YUMING= "https://bipgaming.tvoip.turkcell.com.tr";//BIP正式服
 
     public static final String TEST_HOST_GOOGLE_PAY_URL_NEW=YUMING+"/sb/";
     public static final String BIP_TEST_HOST_GOOGLE_PAY_URL_NEW=BIP_YUMING+"/sb/";
